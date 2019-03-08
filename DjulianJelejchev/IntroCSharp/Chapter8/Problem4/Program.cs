0f30f9f9516d676860dcce6fbc92e4588f8e64e9
JulianZhelezchev <invictus17@abv.bg>
2019-03-08 21:19:00 +0200

    Chapter 8

---
+++ DjulianJelejchev/IntroCSharp/Chapter8/Problem4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Problem4
+{
+    class Program
+    {
+        static void Main()
+        {
+//            Да се напише програма, която преобразува десетично число в двоично.
+//            Правилото е "делим на 2 и долепяме остатъците в обратен ред".
+//За делене с остатък използваме оператора %.Можете да се изхитрите,
+//като използвате Convert.ToString(numDecimal, 2).
+
+          
+            String number = "100"; 
+            int fromBase = 10;
+            int toBase = 2;
+
+            String result = Convert.ToString(Convert.ToInt32(number, fromBase), toBase);
+
+            Console.WriteLine(result); // result 1100100
+
+
+                
+
+
+        }
+    }
+}
